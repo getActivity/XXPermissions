b1efbaef3a61244120e743dec6b88b09b60bd612
Android 轮子哥 <jinqun0730@gmail.com>
2025-06-15 13:41:38 +0800

    优化 PermissionGroups 类中的常量赋值

--- library/src/main/java/com/hjq/permissions/permission/PermissionGroups.java
+++ library/src/main/java/com/hjq/permissions/permission/PermissionGroups.java
@@ -9,12 +9,17 @@ package com.hjq.permissions.permission;
 public final class PermissionGroups {
 
     /**
+     * 权限组的后缀名称
+     */
+    public static final String SUFFIX = "_group";
+
+    /**
      * 存储权限组，包含以下权限
      *
      * {@link PermissionNames#READ_EXTERNAL_STORAGE}
      * {@link PermissionNames#WRITE_EXTERNAL_STORAGE}
      */
-    public static final String STORAGE = "storage";
+    public static final String STORAGE = "storage" + SUFFIX;
 
     /**
      * 日历权限组，包含以下权限
@@ -22,7 +27,7 @@ public final class PermissionGroups {
      * {@link PermissionNames#READ_CALENDAR}
      * {@link PermissionNames#WRITE_CALENDAR}
      */
-    public static final String CALENDAR = "calendar";
+    public static final String CALENDAR = "calendar" + SUFFIX;
 
     /**
      * 通讯录权限组，包含以下权限
@@ -31,7 +36,7 @@ public final class PermissionGroups {
      * {@link PermissionNames#WRITE_CONTACTS}
      * {@link PermissionNames#GET_ACCOUNTS}
      */
-    public static final String CONTACTS = "contacts";
+    public static final String CONTACTS = "contacts" + SUFFIX;
 
     /**
      * 短信权限组，包含以下权限
@@ -42,7 +47,7 @@ public final class PermissionGroups {
      * {@link PermissionNames#RECEIVE_WAP_PUSH}
      * {@link PermissionNames#RECEIVE_MMS}
      */
-    public static final String SMS = "sms";
+    public static final String SMS = "sms" + SUFFIX;
 
     /**
      * 位置权限组，包含以下权限
@@ -59,7 +64,7 @@ public final class PermissionGroups {
      * 注意：在 Android 13 的时候，WIFI 相关的权限已经归到附近设备的权限组了，但是在 Android 13 之前，WIFI 相关的权限归属定位权限组
      * {@link PermissionNames#NEARBY_WIFI_DEVICES}
      */
-    public static final String LOCATION = "location";
+    public static final String LOCATION = "location" + SUFFIX;
 
     /**
      * 传感器权限组，包含以下权限
@@ -67,7 +72,7 @@ public final class PermissionGroups {
      * {@link PermissionNames#BODY_SENSORS}
      * {@link PermissionNames#BODY_SENSORS_BACKGROUND}
      */
-    public static final String SENSORS = "sensors";
+    public static final String SENSORS = "sensors" + SUFFIX;
 
     /**
      * 电话权限组，包含以下权限
@@ -85,7 +90,7 @@ public final class PermissionGroups {
      * {@link PermissionNames#WRITE_CALL_LOG}
      * {@link PermissionNames#PROCESS_OUTGOING_CALLS}
      */
-    public static final String PHONE = "phone";
+    public static final String PHONE = "phone" + SUFFIX;
 
     /**
      * 通话记录权限组（在 Android 9.0 的时候，读写通话记录权限已经归到一个单独的权限组了，但是在 Android 9.0 之前，读写通话记录权限归属电话权限组），包含以下权限
@@ -94,7 +99,7 @@ public final class PermissionGroups {
      * {@link PermissionNames#WRITE_CALL_LOG}
      * {@link PermissionNames#PROCESS_OUTGOING_CALLS}
      */
-    public static final String CALL_LOG = "call_log";
+    public static final String CALL_LOG = "call_log" + SUFFIX;
 
     /**
      * 附近设备权限组，包含以下权限
@@ -107,7 +112,7 @@ public final class PermissionGroups {
      * 注意：在 Android 13 的时候，WIFI 相关的权限已经归到附近设备的权限组了，但是在 Android 13 之前，WIFI 相关的权限归属定位权限组
      * {@link PermissionNames#NEARBY_WIFI_DEVICES}
      */
-    public static final String NEARBY_DEVICES = "nearby_devices";
+    public static final String NEARBY_DEVICES = "nearby_devices" + SUFFIX;
 
     /**
      * 照片和视频权限组（注意：不包含音频权限） ，包含以下权限
@@ -116,5 +121,5 @@ public final class PermissionGroups {
      * {@link PermissionNames#READ_MEDIA_VIDEO}
      * {@link PermissionNames#READ_MEDIA_VISUAL_USER_SELECTED}
      */
-    public static final String IMAGE_AND_VIDEO_MEDIA = "image_and_video_media";
+    public static final String IMAGE_AND_VIDEO_MEDIA = "image_and_video_media" + SUFFIX;
 }
