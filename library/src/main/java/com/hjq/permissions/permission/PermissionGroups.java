23211e148a4595455a9ffa44a0634170d5d0598d
Android 轮子哥 <jinqun0730@gmail.com>
2025-06-15 13:33:33 +0800

    补充 PermissionGroups 类中的代码注释

--- library/src/main/java/com/hjq/permissions/permission/PermissionGroups.java
+++ library/src/main/java/com/hjq/permissions/permission/PermissionGroups.java
@@ -8,33 +8,113 @@ package com.hjq.permissions.permission;
  */
 public final class PermissionGroups {
 
-    /** 存储权限组 */
+    /**
+     * 存储权限组，包含以下权限
+     *
+     * {@link PermissionNames#READ_EXTERNAL_STORAGE}
+     * {@link PermissionNames#WRITE_EXTERNAL_STORAGE}
+     */
     public static final String STORAGE = "storage";
 
-    /** 日历权限组 */
+    /**
+     * 日历权限组，包含以下权限
+     *
+     * {@link PermissionNames#READ_CALENDAR}
+     * {@link PermissionNames#WRITE_CALENDAR}
+     */
     public static final String CALENDAR = "calendar";
 
-    /** 联系人权限组 */
+    /**
+     * 通讯录权限组，包含以下权限
+     *
+     * {@link PermissionNames#READ_CONTACTS}
+     * {@link PermissionNames#WRITE_CONTACTS}
+     * {@link PermissionNames#GET_ACCOUNTS}
+     */
     public static final String CONTACTS = "contacts";
 
-    /** 短信权限组 */
+    /**
+     * 短信权限组，包含以下权限
+     *
+     * {@link PermissionNames#SEND_SMS}
+     * {@link PermissionNames#READ_SMS}
+     * {@link PermissionNames#RECEIVE_SMS}
+     * {@link PermissionNames#RECEIVE_WAP_PUSH}
+     * {@link PermissionNames#RECEIVE_MMS}
+     */
     public static final String SMS = "sms";
 
-    /** 位置权限组 */
+    /**
+     * 位置权限组，包含以下权限
+     *
+     * {@link PermissionNames#ACCESS_COARSE_LOCATION}
+     * {@link PermissionNames#ACCESS_FINE_LOCATION}
+     * {@link PermissionNames#ACCESS_BACKGROUND_LOCATION}
+     *
+     * 注意：在 Android 12 的时候，蓝牙相关的权限已经归到附近设备的权限组了，但是在 Android 12 之前，蓝牙相关的权限归属定位权限组
+     * {@link PermissionNames#BLUETOOTH_SCAN}
+     * {@link PermissionNames#BLUETOOTH_CONNECT}
+     * {@link PermissionNames#BLUETOOTH_ADVERTISE}
+     *
+     * 注意：在 Android 13 的时候，WIFI 相关的权限已经归到附近设备的权限组了，但是在 Android 13 之前，WIFI 相关的权限归属定位权限组
+     * {@link PermissionNames#NEARBY_WIFI_DEVICES}
+     */
     public static final String LOCATION = "location";
 
-    /** 传感器权限组 */
+    /**
+     * 传感器权限组，包含以下权限
+     *
+     * {@link PermissionNames#BODY_SENSORS}
+     * {@link PermissionNames#BODY_SENSORS_BACKGROUND}
+     */
     public static final String SENSORS = "sensors";
 
-    /** 电话权限组 */
+    /**
+     * 电话权限组，包含以下权限
+     *
+     * {@link PermissionNames#READ_PHONE_STATE}
+     * {@link PermissionNames#CALL_PHONE}
+     * {@link PermissionNames#ADD_VOICEMAIL}
+     * {@link PermissionNames#USE_SIP}
+     * {@link PermissionNames#READ_PHONE_NUMBERS}
+     * {@link PermissionNames#ANSWER_PHONE_CALLS}
+     * {@link PermissionNames#ACCEPT_HANDOVER}
+     *
+     * 注意：在 Android 9.0 的时候，读写通话记录权限已经归到一个单独的权限组了，但是在 Android 9.0 之前，读写通话记录权限归属电话权限组
+     * {@link PermissionNames#READ_CALL_LOG}
+     * {@link PermissionNames#WRITE_CALL_LOG}
+     * {@link PermissionNames#PROCESS_OUTGOING_CALLS}
+     */
     public static final String PHONE = "phone";
 
-    /** 通话记录权限组 */
+    /**
+     * 通话记录权限组（在 Android 9.0 的时候，读写通话记录权限已经归到一个单独的权限组了，但是在 Android 9.0 之前，读写通话记录权限归属电话权限组），包含以下权限
+     *
+     * {@link PermissionNames#READ_CALL_LOG}
+     * {@link PermissionNames#WRITE_CALL_LOG}
+     * {@link PermissionNames#PROCESS_OUTGOING_CALLS}
+     */
     public static final String CALL_LOG = "call_log";
 
-    /** 附近设备权限组 */
+    /**
+     * 附近设备权限组，包含以下权限
+     *
+     * 在 Android 12 的时候，蓝牙相关的权限已经归到附近设备的权限组了，但是在 Android 12 之前，蓝牙相关的权限归属定位权限组
+     * {@link PermissionNames#BLUETOOTH_SCAN}
+     * {@link PermissionNames#BLUETOOTH_CONNECT}
+     * {@link PermissionNames#BLUETOOTH_ADVERTISE}
+     *
+     * 注意：在 Android 13 的时候，WIFI 相关的权限已经归到附近设备的权限组了，但是在 Android 13 之前，WIFI 相关的权限归属定位权限组
+     * {@link PermissionNames#NEARBY_WIFI_DEVICES}
+     */
     public static final String NEARBY_DEVICES = "nearby_devices";
 
-    /** 照片和视频权限组（注意：不包含音频权限） */
+    /**
+     * 照片和视频权限组（注意：不包含音频权限） ，包含以下权限
+     *
+     * {@link PermissionNames#READ_MEDIA_IMAGES}
+     * {@link PermissionNames#READ_MEDIA_VIDEO}
+     * {@link PermissionNames#READ_MEDIA_VISUAL_USER_SELECTED}
+     */
     public static final String IMAGE_AND_VIDEO_MEDIA = "image_and_video_media";
 }
