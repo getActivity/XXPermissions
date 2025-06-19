e58bfb7536691d1a5bf90a9f22bf717b8940d80f
Android 轮子哥 <jinqun0730@gmail.com>
2025-06-19 21:12:11 +0800

    优化 Demo 工程中权限拦截器的代码实现

--- app/src/main/java/com/hjq/permissions/demo/permission/PermissionInterceptor.java
+++ app/src/main/java/com/hjq/permissions/demo/permission/PermissionInterceptor.java
@@ -5,9 +5,12 @@ import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Build;
+import android.content.pm.PackageManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import com.hjq.permissions.OnPermissionCallback;
@@ -34,80 +37,31 @@ public final class PermissionInterceptor implements OnPermissionInterceptor {
                                         @NonNull List<IPermission> deniedPermissions, boolean doNotAskAgain,
                                         @Nullable OnPermissionCallback callback) {
         if (callback != null) {
+            // 回调失败给外层监听器
             callback.onDenied(deniedPermissions, doNotAskAgain);
         }
 
-        if (doNotAskAgain) {
-            if (deniedPermissions.size() == 1 && XXPermissions.equalsPermission(deniedPermissions.get(0), PermissionNames.ACCESS_MEDIA_LOCATION)) {
-                Toaster.show(R.string.common_permission_access_media_location_information_hint_fail);
-                return;
-            }
-
-            showPermissionSettingDialog(activity, requestPermissions, deniedPermissions, callback);
+        String permissionHint = generatePermissionHint(activity, requestPermissions, deniedPermissions, doNotAskAgain);
+        if (!doNotAskAgain) {
+            // 如果没有勾选不再询问选项，就弹 Toast 提示给用户
+            Toaster.show(permissionHint);
             return;
         }
 
-        if (deniedPermissions.size() == 1) {
-
-            IPermission deniedPermission = deniedPermissions.get(0);
-
-            String backgroundPermissionOptionLabel = getBackgroundPermissionOptionLabel(activity);
-
-            if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.ACCESS_BACKGROUND_LOCATION)) {
-                Toaster.show(activity.getString(R.string.common_permission_background_location_fail_hint, backgroundPermissionOptionLabel));
-                return;
-            } else if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.BODY_SENSORS_BACKGROUND)) {
-                Toaster.show(activity.getString(R.string.common_permission_background_sensors_fail_hint, backgroundPermissionOptionLabel));
-                return;
-            } else if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.ACCESS_FINE_LOCATION) &&
-                        XXPermissions.containsPermission(requestPermissions, PermissionNames.ACCESS_COARSE_LOCATION)) {
-                // 如果请求的权限中，既包含了精确定位权限，又包含了模糊定位权限，但是用户只同意了模糊定位权限的情况
-                Toaster.show(activity.getString(R.string.common_permission_fine_location_fail_hint));
-                return;
-            }
-        }
-
-        final String message;
-        String permissionNames = PermissionConverter.getNickNamesByPermissions(activity, deniedPermissions);
-        if (!permissionNames.isEmpty()) {
-            message = activity.getString(R.string.common_permission_fail_assign_hint, permissionNames);
-        } else {
-            message = activity.getString(R.string.common_permission_fail_hint);
-        }
-        Toaster.show(message);
+        // 如果勾选了不再询问选项，就弹 Dialog 引导用户去授权
+        showPermissionSettingDialog(activity, requestPermissions, deniedPermissions, callback, permissionHint);
     }
 
-    private void showPermissionSettingDialog(Activity activity, List<IPermission> requestPermissions,
-                                             List<IPermission> deniedPermissions, OnPermissionCallback callback) {
-        if (activity == null || activity.isFinishing() || activity.isDestroyed()) {
+    private void showPermissionSettingDialog(@NonNull Activity activity, @NonNull List<IPermission> requestPermissions,
+                                            @NonNull List<IPermission> deniedPermissions, @Nullable OnPermissionCallback callback,
+                                            @NonNull String permissionHint) {
+        if (activity.isFinishing() || activity.isDestroyed()) {
             return;
         }
 
-        String message = null;
-
-        String permissionNames = PermissionConverter.getNickNamesByPermissions(activity, deniedPermissions);
-        if (!permissionNames.isEmpty()) {
-            if (deniedPermissions.size() == 1) {
-                IPermission deniedPermission = deniedPermissions.get(0);
-
-                if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.ACCESS_BACKGROUND_LOCATION)) {
-                    message = activity.getString(R.string.common_permission_manual_assign_fail_background_location_hint, getBackgroundPermissionOptionLabel(activity));
-                } else if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.BODY_SENSORS_BACKGROUND)) {
-                    message = activity.getString(R.string.common_permission_manual_assign_fail_background_sensors_hint, getBackgroundPermissionOptionLabel(activity));
-                } else if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.ACCESS_FINE_LOCATION) &&
-                            XXPermissions.containsPermission(requestPermissions, PermissionNames.ACCESS_COARSE_LOCATION)) {
-                    // 如果请求的权限中，既包含了精确定位权限，又包含了模糊定位权限，但是用户只同意了模糊定位权限的情况
-                    message = activity.getString(R.string.common_permission_manual_assign_fail_fine_location_hint);
-                }
-            }
-            if (TextUtils.isEmpty(message)) {
-                message = activity.getString(R.string.common_permission_manual_assign_fail_hint, permissionNames);
-            }
-        } else {
-            message = activity.getString(R.string.common_permission_manual_fail_hint);
-        }
-
-        showDialog(activity, activity.getString(R.string.common_permission_alert), message, true, activity.getString(R.string.common_permission_goto_setting_page), (dialog, which) -> {
+        String dialogTitle  = activity.getString(R.string.common_permission_alert);
+        String confirmButtonText = activity.getString(R.string.common_permission_go_to_authorization);
+        DialogInterface.OnClickListener confirmListener = (dialog, which) -> {
             dialog.dismiss();
             XXPermissions.startPermissionActivity(activity, deniedPermissions, new OnPermissionPageCallback() {
 
@@ -116,16 +70,76 @@ public final class PermissionInterceptor implements OnPermissionInterceptor {
                     if (callback == null) {
                         return;
                     }
+                    // 用户全部授权了，回调成功给外层监听器，免得用户还要再发起权限申请
                     callback.onGranted(requestPermissions, true);
                 }
 
                 @Override
                 public void onDenied() {
-                    showPermissionSettingDialog(activity, requestPermissions,
-                        XXPermissions.getDeniedPermissions(activity, requestPermissions), callback);
+                    List<IPermission> latestDeniedPermissions = XXPermissions.getDeniedPermissions(activity, requestPermissions);
+                    // 递归显示对话框，让提示用户授权，只不过对话框是可取消的，用户不想授权了，随时可以点击返回键或者对话框蒙层来取消显示
+                    showPermissionSettingDialog(activity, requestPermissions, latestDeniedPermissions, callback,
+                        generatePermissionHint(activity, requestPermissions, latestDeniedPermissions, true));
                 }
             });
-        });
+        };
+
+        // 另外这里需要判断 Activity 的类型来申请权限，这是因为只有 AppCompatActivity 才能调用 Support 包的 AlertDialog 来显示，否则会出现报错
+        // java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity
+        // 为什么不直接用 App 包 AlertDialog 来显示，而是两套规则？因为 App 包 AlertDialog 是系统自带的类，不同 Android 版本展现的样式可能不太一样
+        // 如果这个 Android 版本比较低，那么这个对话框的样式就会变得很丑，准确来讲也不能说丑，而是当时系统的 UI 设计就是那样，它只是跟随系统的样式而已
+        Dialog dialog;
+        if (activity instanceof AppCompatActivity) {
+            dialog = new AlertDialog.Builder(activity)
+                .setTitle(dialogTitle)
+                .setMessage(permissionHint)
+                // 这里需要设置成可取消的，这样用户不想授权了，随时可以点击返回键或者对话框蒙层来取消显示 Dialog
+                .setCancelable(true)
+                .setPositiveButton(confirmButtonText, confirmListener)
+                .create();
+        } else {
+            dialog = new Builder(activity)
+                .setTitle(dialogTitle)
+                .setMessage(permissionHint)
+                // 这里需要设置成可取消的，这样用户不想授权了，随时可以点击返回键或者对话框蒙层来取消显示 Dialog
+                .setCancelable(true)
+                .setPositiveButton(confirmButtonText, confirmListener)
+                .create();
+        }
+        dialog.show();
+        // 将 Activity 和 Dialog 生命周期绑定在一起，避免可能会出现的内存泄漏
+        // 当然如果上面创建的 Dialog 已经有做了生命周期管理，则不需要执行下面这行代码
+        WindowLifecycleManager.bindDialogLifecycle(activity, dialog);
+    }
+
+    /**
+     * 生成权限提示文案
+     */
+    @NonNull
+    private String generatePermissionHint(@NonNull Activity activity, @NonNull List<IPermission> requestPermissions,
+                                            @NonNull List<IPermission> deniedPermissions, boolean doNotAskAgain) {
+        if (deniedPermissions.size() == 1) {
+            IPermission deniedPermission = deniedPermissions.get(0);
+            if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.ACCESS_BACKGROUND_LOCATION)) {
+                return activity.getString(doNotAskAgain ? R.string.common_permission_background_location_fail_hint_1 :
+                                                            R.string.common_permission_background_location_fail_hint_2,
+                                                            getBackgroundPermissionOptionLabel(activity));
+            } else if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.BODY_SENSORS_BACKGROUND)) {
+                return activity.getString(doNotAskAgain ? R.string.common_permission_background_sensors_fail_hint_1 :
+                                                            R.string.common_permission_background_sensors_fail_hint_2,
+                                                            getBackgroundPermissionOptionLabel(activity));
+            } else if (XXPermissions.equalsPermission(deniedPermission, PermissionNames.ACCESS_FINE_LOCATION) &&
+                XXPermissions.containsPermission(requestPermissions, PermissionNames.ACCESS_COARSE_LOCATION)) {
+                // 如果请求的权限中，既包含了精确定位权限，又包含了模糊定位权限，但是用户只同意了模糊定位权限的情况
+                return activity.getString(doNotAskAgain ? R.string.common_permission_fine_location_fail_hint_1 :
+                                                            R.string.common_permission_fine_location_fail_hint_2,
+                                                            getBackgroundPermissionOptionLabel(activity));
+            }
+        }
+
+        return activity.getString(doNotAskAgain ? R.string.common_permission_fail_assign_hint_1 :
+                                                R.string.common_permission_fail_assign_hint_2,
+                                                PermissionConverter.getNickNamesByPermissions(activity, deniedPermissions));
     }
 
     /**
@@ -133,61 +147,13 @@ public final class PermissionInterceptor implements OnPermissionInterceptor {
      */
     @NonNull
     private String getBackgroundPermissionOptionLabel(Context context) {
-        String backgroundPermissionOptionLabel = "";
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            backgroundPermissionOptionLabel = String.valueOf(context.getPackageManager().getBackgroundPermissionOptionLabel());
+        PackageManager packageManager = context.getPackageManager();
+        if (packageManager != null && VERSION.SDK_INT >= VERSION_CODES.R) {
+            CharSequence backgroundPermissionOptionLabel = packageManager.getBackgroundPermissionOptionLabel();
+            if (!TextUtils.isEmpty(backgroundPermissionOptionLabel)) {
+                return backgroundPermissionOptionLabel.toString();
+            }
         }
-        if (TextUtils.isEmpty(backgroundPermissionOptionLabel)) {
-            backgroundPermissionOptionLabel = context.getString(R.string.common_permission_background_default_option_label);
-        }
-        return backgroundPermissionOptionLabel;
-    }
-
-    private void showDialog(@NonNull Activity activity, @Nullable String dialogTitle, @Nullable String dialogMessage, boolean dialogCancelable,
-                            @Nullable String confirmButtonText, @Nullable DialogInterface.OnClickListener confirmListener) {
-        showDialog(activity, dialogTitle, dialogMessage, dialogCancelable, confirmButtonText, confirmListener, null, null);
-    }
-
-    /**
-     * 显示对话框
-     *
-     * @param activity                  Activity 对象
-     * @param dialogTitle               对话框标题
-     * @param dialogMessage             对话框消息
-     * @param dialogCancelable          对话框是否可取消
-     * @param confirmButtonText         对话框确认按钮文本
-     * @param confirmListener           对话框确认按钮点击事件
-     * @param cancelButtonText          对话框取消按钮文本
-     * @param cancelListener            对话框取消按钮点击事件
-     */
-    private void showDialog(@NonNull Activity activity, @Nullable String dialogTitle, @Nullable String dialogMessage, boolean dialogCancelable,
-                            @Nullable String confirmButtonText, @Nullable DialogInterface.OnClickListener confirmListener,
-                            @Nullable String cancelButtonText, @Nullable DialogInterface.OnClickListener cancelListener) {
-        // 另外这里需要判断 Activity 的类型来申请权限，这是因为只有 AppCompatActivity 才能调用 Support 包的 AlertDialog 来显示，否则会出现报错
-        // java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity
-        // 为什么不直接用 App 包 AlertDialog 来显示，而是两套规则？因为 App 包 AlertDialog 是系统自带的类，不同 Android 版本展现的样式可能不太一样
-        // 如果这个 Android 版本比较低，那么这个对话框的样式就会变得很丑，准确来讲也不能说丑，而是当时系统的 UI 设计就是那样，它只是跟随系统的样式而已
-        Dialog dialog;
-        if (activity instanceof AppCompatActivity) {
-            dialog = new android.support.v7.app.AlertDialog.Builder(activity)
-                .setTitle(dialogTitle)
-                .setMessage(dialogMessage)
-                .setCancelable(dialogCancelable)
-                .setPositiveButton(confirmButtonText, confirmListener)
-                .setNegativeButton(cancelButtonText, cancelListener)
-                .create();
-        } else {
-            dialog = new Builder(activity)
-                .setTitle(dialogTitle)
-                .setMessage(dialogMessage)
-                .setCancelable(dialogCancelable)
-                .setPositiveButton(confirmButtonText, confirmListener)
-                .setNegativeButton(cancelButtonText, cancelListener)
-                .create();
-        }
-        dialog.show();
-        // 将 Activity 和 Dialog 生命周期绑定在一起，避免可能会出现的内存泄漏
-        // 当然如果上面创建的 Dialog 已经有做了生命周期管理，则不需要执行下面这行代码
-        WindowLifecycleManager.bindDialogLifecycle(activity, dialog);
+        return context.getString(R.string.common_permission_background_default_option_label);
     }
 }
