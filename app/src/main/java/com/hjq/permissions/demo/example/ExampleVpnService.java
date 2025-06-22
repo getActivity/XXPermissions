5569a60fbdc3606b4f5e79b959040553cc3ab9c6
Android 轮子哥 <jinqun0730@gmail.com>
2025-06-22 20:59:45 +0800

    补充 VPN 权限的错误检查机制

---
+++ app/src/main/java/com/hjq/permissions/demo/example/ExampleVpnService.java
@@ -0,0 +1,7 @@
+package com.hjq.permissions.demo.example;
+
+import android.net.VpnService;
+
+public final class ExampleVpnService extends VpnService {
+
+}
--- library/src/main/java/com/hjq/permissions/permission/special/BindVpnServicePermission.java
+++ library/src/main/java/com/hjq/permissions/permission/special/BindVpnServicePermission.java
@@ -1,15 +1,22 @@
 package com.hjq.permissions.permission.special;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.VpnService;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.hjq.permissions.manifest.AndroidManifestInfo;
+import com.hjq.permissions.manifest.node.PermissionManifestInfo;
+import com.hjq.permissions.manifest.node.ServiceManifestInfo;
+import com.hjq.permissions.permission.base.IPermission;
 import com.hjq.permissions.tools.AndroidVersionTools;
 import com.hjq.permissions.tools.PermissionUtils;
 import com.hjq.permissions.permission.PermissionNames;
 import com.hjq.permissions.permission.common.SpecialPermission;
+import java.util.List;
 
 /**
  *    author : Android 轮子哥
@@ -68,5 +75,34 @@ public final class BindVpnServicePermission extends SpecialPermission {
             intent = getAndroidSettingAppIntent();
         }
         return intent;
+    }
+
+    @Override
+    protected void checkSelfByManifestFile(@NonNull Activity activity,
+                                            @NonNull List<IPermission> requestPermissions,
+                                            @NonNull AndroidManifestInfo androidManifestInfo,
+                                            @NonNull List<PermissionManifestInfo> permissionManifestInfoList,
+                                            @Nullable PermissionManifestInfo currentPermissionManifestInfo) {
+        super.checkSelfByManifestFile(activity, requestPermissions, androidManifestInfo, permissionManifestInfoList, currentPermissionManifestInfo);
+        // 判断有没有 Service 类注册了 android:permission="android.permission.BIND_VPN_SERVICE" 属性
+        List<ServiceManifestInfo> serviceManifestInfoList = androidManifestInfo.serviceManifestInfoList;
+        for (int i = 0; i < serviceManifestInfoList.size(); i++) {
+            String permission = serviceManifestInfoList.get(i).permission;
+            if (permission == null) {
+                continue;
+            }
+            if (PermissionUtils.equalsPermission(getPermissionName(), permission)) {
+                // 发现有 Service 注册过，终止循环并返回，避免走到抛异常的情况
+                return;
+            }
+        }
+
+        /*
+         没有找到有任何 Service 注册过 android:permission="android.permission.BIND_VPN_SERVICE" 属性，
+         请注册该属性给 VpnService 的子类到 AndroidManifest.xml 文件中
+         */
+        throw new IllegalArgumentException("No Service was found to have registered the android:permission=\"" + getPermissionName() +
+            "\" property, Please register this property to VpnService subclass by AndroidManifest.xml file, "
+            + "otherwise it will lead to can't apply for the permission");
     }
 }
